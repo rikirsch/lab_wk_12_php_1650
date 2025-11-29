265531f039e639b8dd0168ca09276b951fed2847
eydub7 <emmett_dubnoff@brown.edu>
2025-11-29 16:10:23 +0000

    beginnings of thinning as well as progress for runaday

---
+++ Thinning.R
@@ -0,0 +1,9 @@
+#thinning function 
+
+#this one is pretty simple so i will add documentation for later
+thinning <- function (current_cumulated_time, arrival_rates) {
+  #find which set of hours the time belongs to, store the lower value as interval
+  interval<-floor(current_cumulated_time) 
+  
+  
+}
